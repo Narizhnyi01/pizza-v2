d8dde9f5a3a84fa0744b864296d9216808eecc23
Narizhnyi01 <narizhnyi01@gmail.com>
2022-05-26 17:01:30 +0300

    adding components and responsive design

---
+++ src/components/Categories.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+
+function Categories() {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const categoties = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые'];
+
+  const onClickCategory = (index) => {
+    setActiveIndex(index);
+  };
+
+  return (
+    <div className="categories">
+      <ul>
+        {categoties.map((value, index) => {
+          return (
+            <li
+              key={index}
+              onClick={() => onClickCategory(index)}
+              className={activeIndex === index ? 'active' : ''}>
+              {value}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
+
+export default Categories;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import Categories from '../components/Categories';
+import Sort from '../components/Sort';
+import PizzaBlock from '../components/PizzaBlock';
+import { Skeleton } from '../components/PizzaBlock/Skeleton';
+
+function Home() {
+  const [items, setItems] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    fetch('https://628dee16368687f3e70c810e.mockapi.io/items')
+      .then((res) => res.json())
+      .then((data) => {
+        setItems(data);
+        setIsLoading(false);
+      });
+    window.scrollTo(0, 0);
+  }, []);
+
+  return (
+    <div className="container">
+      <div className="content__top">
+        <Categories />
+        <Sort />
+      </div>
+      <h2 className="content__title">Все пиццы</h2>
+      <div className="content__items">
+        {isLoading
+          ? [...new Array(6)].map((_, i) => <Skeleton key={i} />)
+          : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
+      </div>
+    </div>
+  );
+}
+
+export default Home;
